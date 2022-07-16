a00c7996ca7430e87dc14288907c1a82b4436cec
anjali7528 <anjaligandhi248@gmail.com>
2022-07-16 17:30:20 +0530

    about

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -6,54 +6,68 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import './about.css'
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
 
 const About = () => {
     const theme = useTheme();
   return (
-<div>
-{/* <Card sx={{ display: 'flex'  }}>
+    <div className="main1"> 
+<div className='main'>
+<div className='about'>
+<CardMedia
+      component="img"
+      sx={{ width: 300, position: "relative",
+        top: 0,
+        left: 0, borderRadius:1}}
+      className="img3"
+      image="https://images.unsplash.com/photo-1586596436603-cb5e2445b06d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+      alt="Live from space album cover"
+    />
+
+ <Card sx={{ display: 'flex' }}>
         <CardMedia
       component="img"
-      sx={{ width: 151 }}
+      sx={{ width: 270, position: "absolute",
+        top: 25,
+        left: -20,borderRadius:1,}}
+      className='img2'
       image="https://images.unsplash.com/photo-1573052775154-df4d35b5b5ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
       alt="Live from space album cover"
     />
-    <Box sx={{ display: 'flex', flexDirection: 'column'}}>
-      <CardContent sx={{ flex: '1 0 auto' }}>
-        <Typography component="div" variant="h5">
-          Live From Space
+    </Card>
+  </div>
+</div >
+<Card sx={{ minWidth: 275, height: 447,marginTop: 19, boxShadow:'none' }}>
+<CardContent style={{padding:40}}>
+        <Typography variant="h4" component="div">
+           Welcome To <br/>Sushobhit Printers
         </Typography>
-        <Typography variant="subtitle1" color="text.secondary" component="div">
-          Mac Miller
+        <Typography variant="body2" style={{paddingTop: 30}}>
+          One of the pioneer printing and packaging 
+          <br/>
+          firm in India, we have attained prominance as  
+          <br/>
+          the leader of the field of printing and 
+          <br/> packging through, competivness and timely <br/>
+          delivery with highest 
+          quality standards and <br/>recorgnized safety performance. 
+          <br/><br/><br/>
+          In the era of advanced technology and new <br/>techniques, 
+          we continously
+          strive for innovation <br/>and enhanced efficiency. 
         </Typography>
       </CardContent>
-    </Box> */}
-
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
     </Card>
-</div>
+    </div>
     
   )
 }
